c535233b8727a42cdfd1d18fa63778350b13d347
mohammedansil <anzilmohd4@gmail.com>
2022-06-21 18:12:38 +0530

    HeroSection

--- components/HeroSection/index.jsx
+++ components/HeroSection/index.jsx
@@ -1,16 +1,28 @@
 
 import MainButton, { FourthButton, SecondButton, ThirdButton } from "../Button"
-import { Heading } from "./HeroSection.styled"
+import { Heading,Hero,HeroDescription,HeroButtons,ImageContainer,Image ,ImageBelow } from "./HeroSection.styled"
 
 
 const HeroSection = () => {
     return (
       <>
-      <Heading>Main Heading</Heading>
-      <MainButton>Main Button</MainButton>
-      <SecondButton>Second Button</SecondButton>
-      <ThirdButton>Third Button</ThirdButton>
-      <FourthButton>Fourth Button</FourthButton>
+      <Hero>
+
+        <Heading>IT'S TIME TO PURSUR BETTER FUTURE</Heading>
+        <HeroDescription>We Provide financial planning, wealth managment and investment services</HeroDescription>
+        
+        <HeroButtons>
+          <SecondButton>Lets Talk</SecondButton>
+          <FourthButton>Learn More</FourthButton>
+        </HeroButtons>
+        <ImageContainer>
+          <Image src="https://images.unsplash.com/photo-1579567761406-4684ee0c75b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"/>
+          <ImageBelow src="https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"/>
+          <Image src="https://images.unsplash.com/photo-1595327656903-2f54e37ce09b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"/>
+        </ImageContainer>
+        
+        
+      </Hero>
       </>
     )
   }
