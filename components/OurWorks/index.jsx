7d3073a056650878ac12d8de43397e97aeb86293
mohammedansil <anzilmohd4@gmail.com>
2022-06-21 18:14:09 +0530

    OurWorks

--- components/OurWorks/index.jsx
+++ components/OurWorks/index.jsx
@@ -1,10 +1,30 @@
-import { Heading } from "./OurWorks.styled"
+import { Heading,WorkTitle,Work,WorkOne,WorkLinkImage,WorkTwo,WorkThree } from "./OurWorks.styled"
 
 
 const OurWorks = () => {
     return (
       <>
-      <Heading>Our Works</Heading>
+      <OurWorks>
+        <WorkOne>
+          <WorkTitle>Latest Projects</WorkTitle>
+          <Work>
+
+          </Work>
+        </WorkOne>
+        <WorkTwo>
+          <Work>
+              
+          </Work>
+        </WorkTwo>
+        <WorkThree>
+          <Work>
+              
+          </Work>
+          <WorkLinkImage>
+
+          </WorkLinkImage>
+        </WorkThree>
+      </OurWorks>
       </>
     )
   }
