fc708b115e6d151858679739860ab794eb4ca36f
mohammedansil <anzilmohd4@gmail.com>
2022-06-21 18:12:50 +0530

    About US

--- components/AboutUs/AboutUs.styled.js
+++ components/AboutUs/AboutUs.styled.js
@@ -1,7 +1,42 @@
 import styled from "styled-components";
-import { HeadingH3 } from "../cssHelper";
+import { HeadingH2,flexRowCenter,flexColumnCenter } from "../cssHelper";
 
-export const Heading = styled.h1`
- ${ HeadingH3 };
+
+export const About  = styled.div`
+margin-top: 150px;
+`
+export const AboutTitle = styled.h1`
+ font-size: 300px;
+ text-transform: uppercase;
 `;
 
+export const AboutContent = styled.div`
+${flexColumnCenter}
+height: 60vh;
+position: relative;
+width: 100%;
+`
+export const AboutDescription = styled.div`
+font-size: 50px;
+width: 87%;
+position: absolute;
+top: 50px;
+text-align: right;
+`
+export const AboutQuote = styled.div`
+margin-top:150px;
+`
+export const AboutLink = styled.div`
+position: absolute;
+right:50px;
+bottom:0;
+`
+export const AboutImage = styled.img`
+width:500px;
+height:750px;
+margin-top:50px;
+`
+export const AboutBelow = styled.div`
+${flexRowCenter}
+width: 95%;
+`
