3da25155edd2ba5c58e6e0cbb2fb7e0d4a5eb7c2
Junaid S <intelpik@gmail.com>
2022-06-29 23:36:27 +0530

    comments in faq cleared

--- components/FAQ/index.jsx
+++ components/FAQ/index.jsx
@@ -1,48 +1,8 @@
-import {
-  Heading,
-  Faq,
-  FaqsSection,
-  Container,
-  LeftSection,
-  RightSection,
-  FaqIndex,
-  FaqQuestion,
-  FaqAnswer,
-  Icon,
-  Section,
-} from "./FAQ.styled";
 import FaqLayout from "./faqLayout";
 import data from '../../data/faqData';
 
 const FAQS = () => {
   return (
-    // <Section>
-    //   <Container>
-
-
-    //     <FaqsSection>
-
-    //       <LeftSection>
-
-    //         <Faq>
-    //           <FaqQuestion>Services can also be a more stable</FaqQuestion>
-    //           <FaqAnswer>
-    //             Table source of profits, as they tend not to be as exposed to
-    //             fluctuating bussiness
-    //           </FaqAnswer>
-    //         </Faq>
-
-    //       </LeftSection>
-
-    //       <RightSection>
-    //         <Icon>-</Icon>
-    //       </RightSection>
-
-    //     </FaqsSection>
-        
-
-    //   </Container>
-    // </Section>
 
     <FaqLayout>
       <FaqLayout.Header>Frequently Asked Questions</FaqLayout.Header>
