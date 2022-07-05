56d0324145fa3719fcb91ee3bf79447e10f6e548
Junaid S <intelpik@gmail.com>
2022-07-05 11:04:55 +0530

    small change added in loading screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,70 +1,46 @@
-
-import React,{useState,useEffect} from 'react';
-import { useRouter } from 'next/router'
+import { useState, useEffect } from "react";
+import { useRouter } from 'next/router';
 import { ThemeProvider } from "styled-components";
 import { theme } from "../components/Theme";
 import Layout from "../components/layout";
 import GlobalStyle from "../components/globalStyles";
 import LoadingScreen from "../components/LoadingScreen";
+// import Router from 'next/router';
+// import NProgress from 'nprogress'; //nprogress module
+// import 'nprogress/nprogress.css'; //styles of nprogress
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    // setLoading(true);
-    setTimeout(() => setLoading(true), 2000);
-  }, []);
-  // const router = useRouter();
-
   // const [loading, setLoading] = useState(false);
 
   // useEffect(() => {
-  //     const handleStart = (url) => (url !== router.asPath) && setLoading(true);
-  //     const handleComplete = (url) => (url === router.asPath) && setLoading(false);
-
-  //     router.events.on('routeChangeStart', handleStart)
-  //     router.events.on('routeChangeComplete', handleComplete)
-  //     router.events.on('routeChangeError', handleComplete)
-
-  //     return () => {
-  //         router.events.off('routeChangeStart', handleStart)
-  //         router.events.off('routeChangeComplete', handleComplete)
-  //         router.events.off('routeChangeError', handleComplete)
-  //     }
-  // })
-
-  // useEffect(() => {
-  //   const handleStart = (url) => {
-  //     url !== router.pathname ? setLoading(true) : setLoading(false);
-  //   };
-  //   const handleComplete = (url) => setLoading(false);
-
-  //   router.events.on("routeChangeStart", handleStart);
-  //   router.events.on("routeChangeComplete", handleComplete);
-  //   router.events.on("routeChangeError", handleComplete);
-  // }, [router]);
-
-  // useEffect(() => {
-  //   const handleStart = () => { setLoading(true); };
-  //   const handleComplete = () => { setLoading(false); };
-
-  //   router.events.on('routeChangeStart', handleStart);
-  //   router.events.on('routeChangeComplete', handleComplete);
-  //   router.events.on('routeChangeError', handleComplete);
-  // }, [router]);
+  //   // setLoading(true);
+  //   setTimeout(() => setLoading(true), 2000);
+  // }, []);
+  // Router.events.on('routeChangeStart', () => NProgress.start());
+  //  Router.events.on('routeChangeComplete', () => NProgress.done()); 
+  //  Router.events.on('routeChangeError', () => NProgress.done());
+  const router = useRouter();
+    const [pageLoading, setPageLoading] = useState(false);
+    useEffect(() => {
+        const handleStart = () => { setPageLoading(true); };
+        const handleComplete = () => { setPageLoading(false); };
+    
+        router.events.on('routeChangeStart', handleStart);
+        router.events.on('routeChangeComplete', handleComplete);
+        router.events.on('routeChangeError', handleComplete);
+      }, [router]);
 
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        {!loading ? (
-           <LoadingScreen />
-       ) : (
-       
+        {pageLoading ? (
+          <LoadingScreen />
+        ) : (
           <Layout>
-          <Component {...pageProps} />
-        </Layout>
-        )} 
+            <Component {...pageProps} />
+          </Layout>
+       )}
       </ThemeProvider>
     </>
   );
