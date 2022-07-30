f65f7a8748f5e30963831be708015cfaf69df8dc
Junaid S <intelpik@gmail.com>
2022-07-30 20:59:50 +0530

    favicon

--- pages/index.js
+++ pages/index.js
@@ -44,6 +44,14 @@ export default function Home() {
           content="Intelpik is a freelance team of industry experts  providing digital solutions like Web Development,App Development and Branding."
         />
         <meta property="twitter:image" content="/images/banner.jpeg"></meta>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+<link rel="manifest" href="/site.webmanifest"/>
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+<meta name="msapplication-TileColor" content="#da532c"/>
+<meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <HomePage />
     </>
