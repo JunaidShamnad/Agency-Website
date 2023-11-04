7a621a5001076b6e6c03649a61a9ec7a8a706ec1
Junaid <intelpik@gmail.com>
2023-11-04 12:22:42 +0530

    Update index.jsx

--- components/OurWorks/index.jsx
+++ components/OurWorks/index.jsx
@@ -83,7 +83,7 @@ const OurWorks = () => {
               <WorkDiv key={work.id} style={{paddingRight:"10px"}}>
               <ImageDiv 
                 style={{background:`url(${work.image})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
-                <Link href={work.url} as="a" target="_blank" passHref>
+                <Link as="a" target="_blank" passHref>
                   <ViewButton onClick={() => { window.open(work.url, '_blank'); }}> {work.name}</ViewButton> 
                 </Link>
               </ImageDiv>
