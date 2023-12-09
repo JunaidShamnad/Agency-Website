49a18618c66bf57af8412f6c7b7f02e5938092f9
Junaid <intelpik@gmail.com>
2023-12-09 14:15:01 +0530

    Update LoadingScreen.styled.js

--- components/LoadingScreen/LoadingScreen.styled.js
+++ components/LoadingScreen/LoadingScreen.styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Screen = styled.div`
   position:relative;
   width: 100%;
-  height: 100vh;
+  height: 100svh;
   background: #5928E5;
   display: flex;
   justify-content: center;
