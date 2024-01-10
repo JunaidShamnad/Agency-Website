f22f2c8cf017e6a18158889a125f93bdb8514c1c
Junaid <intelpik@gmail.com>
2024-01-10 23:10:10 +0530

    Update OurWorkData.js -- added new client in ourwork data

--- components/OurWorks/OurWorkData.js
+++ components/OurWorks/OurWorkData.js
@@ -1,54 +1,60 @@
 const works = [
     {
         id: 1,
+        name: 'Prolifica',
+        image: '/images/MOCKUPS1/Prolifica-mockup7.png',
+        url: 'https://www.prolificatech.com/'
+    },
+    {
+        id: 2,
         name: 'Aadiveda Ayurveda',
         image: '/images/MOCKUPS1/aadiveda-mockup1.png',
         url: 'https://www.aadivedaayurveda.com/'
     },
     {
-        id: 2,
+        id: 3,
         name: 'Hitoq',
         image: '/images/MOCKUPS1/hitoq-mockup3.png',
         url: 'http://www.hitoq.com',
         alt: 'Preview of Hitoq website'
     },
     {
-        id: 3,
+        id: 4,
         name: 'Bluecon',
         image: '/images/MOCKUPS1/bluecon-mockup4.png',
         url: 'https://www.blueconllc.com/',
         alt: 'Preview of Bluecon website'
     },
     {
-        id: 4,
+        id: 5,
         name: 'Neocart',
         image: '/images/MOCKUPS1/neocart-mockup5.png',
         url: 'https://neocart.ae/',
         alt: 'Preview of Neocart website'
     },
     {
-        id: 5,
+        id: 6,
         name: 'Bellkare',
         image: '/images/MOCKUPS1/Bellkare-mockup6.png',
         url: 'https://www.bellkare.com/',
         alt: 'Preview of Bellkare website'
     },
     {
-        id: 6,
+        id: 7,
         name: 'Neha Group',
         image: '/images/MOCKUPS1/NehaGroup-mockup7.png',
         url: 'https://nehagrp.com/',
         alt: 'Screenshot of Nehagroup website'
     },
     {
-        id: 7,
+        id: 78,
         name: 'Innovgrab',
         image: '/images/MOCKUPS1/Innovgrab-mockup8.png',
         url: 'https://www.innovgrab.com/',
         alt: 'Preview of Innovgrab website'
     },
     {
-        id: 8,
+        id: 9,
         name: "Dowl",
         image: 'images/MOCKUPS1/Dowl-mockup2.png',
         url: 'https://www.dowlfabs.com/',
